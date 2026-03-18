35d637a7538e75dbdb671e8f361c210caa2018d4
CalamitySerenade <wilsongarate1@gmail.com>
2026-03-18 14:52:20 -0400

    Work in progress

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-import {db} from "firebase-config.js"
+import {db} from "./firebase-config.js"
+import {doc,collection,deleteDoc,updateDoc,getDoc} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
+
+
+
 const deleteButton=document.getElementById("delete");
 deleteButton.addEventListener("click", async()=>{
     const taskID=document.getElementById("taskID").value;
