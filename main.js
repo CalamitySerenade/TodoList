e5a6c47f6e7408bbc368c3a29a9d36a1cd274ed1
CalamitySerenade <wilsongarate1@gmail.com>
2026-03-19 08:35:03 -0400

    functions

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 import {db} from "./firebase-config.js"
-import {doc,collection,deleteDoc,updateDoc,getDoc} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
+import {doc,collection,deleteDoc,updateDoc,getDocs} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js"
 
-
+async function displayAllTasks(){
+    try{
+        const querySnapshot=await getDocs(collection(db,"tasks"));
+        const tasks = [];
+        querySnapshot.forEach((doc)=>{
+            tasks.push({
+                id: doc.id,
+                ...doc.data()
+            });
+        })
+        const list=document.getElementById("list")
+        if(list){
+            tasks.forEach((task)=>{
+                const li=document.createElement("li");
+                li.textContent=task.name + task.date + task.status + task.priority
+            })
+        }
+        return tasks
+    }catch (error){
+        console.error("Error with Task Display", error);
+    }
+}
 
 const deleteButton=document.getElementById("delete");
 deleteButton.addEventListener("click", async()=>{
