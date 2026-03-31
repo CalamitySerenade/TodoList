72377c8478195d80a0d4eb136303e5549a042737
CalamitySerenade <wilsongarate1@gmail.com>
2026-03-31 09:40:01 -0400

    Added firebase config and git ignore

--- StudiumProject/main.js
+++ StudiumProject/main.js
@@ -1,11 +1,23 @@
 const inputCreate=document.getElementById("name");
+const inputJoin=document.getElementById("")
+const ul=document.getElementById("testing");
 const create=document.getElementById("createBtn");
-create.addEventListener("click", async()=>{
-    
+create.addEventListener("click", async(e)=>{
+    e.preventDefault();
+    const studyboardCreate=document.createElement("li");
+    const studyboardCreateDisplay=document.createTextNode(inputCreate.value);
+    studyboardCreate.appendChild(studyboardCreateDisplay);
+    document.getElementById("testing").appendChild(studyboardCreate);
     alert("Studyboard created sucessfully!");
+    inputCreate.value="";
 })
 
 const join=document.getElementById("joinBtn");
-join.addEventListener("click", async()=>{
+join.addEventListener("click", async(e)=>{
+    e.preventDefault();
+    const studyboardJoin=document.createElement("li");
+    const studyboardJoinDisplay=document.createTextNode(inputCreate.value);
+    studyboardCreate.appendChild(studyboardCreateDisplay);
+    document.getElementById("testing").appendChild(studyboardCreate);
     alert("Sucessfully joined a studyboard!")
 })
