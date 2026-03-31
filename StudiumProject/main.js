8fd792f6f82a8df2a1c8513f159a1bbdd196e70c
CalamitySerenade <wilsongarate1@gmail.com>
2026-03-31 09:15:35 -0400

    Updates to html

---
+++ StudiumProject/main.js
@@ -0,0 +1,11 @@
+const inputCreate=document.getElementById("name");
+const create=document.getElementById("createBtn");
+create.addEventListener("click", async()=>{
+    
+    alert("Studyboard created sucessfully!");
+})
+
+const join=document.getElementById("joinBtn");
+join.addEventListener("click", async()=>{
+    alert("Sucessfully joined a studyboard!")
+})
